efb289b40b167ab97dca8fb1289c7d86cf1fd520
vasco ceti <vasco.ceti@gmail.com>
2024-01-08 18:04:37 +0100

    8gen2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import Player from "./components/Player";
 
 function App() {
-	return (
-		<main>
-			<div id='game-container'>
-				<ol id='players'>
-					<Player
-						name='Player 1'
-						symbol='X'
-					/>
-					<Player
-						name='Player 2'
-						symbol='O'
-					/>
-				</ol>
-				gameboard
-			</div>
-			log
-		</main>
-	);
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        gameboard
+      </div>
+      log
+    </main>
+  );
 }
 
 export default App;
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,28 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
-	const [isEditing, setIsEditing] = useState(false);
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
-	return (
-		<li>
-			<span className='player'>
-				{isEditing ? (
-					<input
-						type='text'
-						value={name}
-						required
-					/>
-				) : (
-					<span className='player-name'>{name}</span>
-				)}
+  return (
+    <li>
+      <span className="player">
+        {isEditing ? (
+          <input
+            type="text"
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
+            required
+          />
+        ) : (
+          <span className="player-name">{playerName}</span>
+        )}
 
-				<span className='player-symbol'>{symbol}</span>
-			</span>
-			<button
-				className='button'
-				onClick={() => setIsEditing(!isEditing)}>
-				Edit
-			</button>
-		</li>
-	);
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button className="button" onClick={() => setIsEditing((prev) => !prev)}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
 }
